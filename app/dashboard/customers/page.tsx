3729ebbbe6e0b00e5d37a3bcb9ccf6a6889f920b
Lokesh Pawar <lokeshpawar921@gmail.com>
2025-09-18 02:17:30 +0530

    updated customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,25 @@
-export default function Customers() {
-  return <>Customers</>;
+import { fetchFilteredCustomers } from "@/app/lib/data";
+import CustomersTable from "@/app/ui/customers/table";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Customers",
+};
+
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+    page?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || "";
+
+  const customers = await fetchFilteredCustomers(query);
+
+  return (
+    <main>
+      <CustomersTable customers={customers} />
+    </main>
+  );
 }
